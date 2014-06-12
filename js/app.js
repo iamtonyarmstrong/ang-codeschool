5a7194c7a0a8047f58a890befd7cfa81d9e276d3
Tony Armstrong <i.am@iamtonyarmstrong.com>
2014-06-12 10:19:13 -0400

    Angular filters and more directives

--- js/app.js
+++ js/app.js
@@ -1,2 +1,38 @@
-console.log("starting angular");
-var app = angular.module('store',[]);
+(function(){
+    console.log("starting angular");
+    var app = angular.module('store',[]);
+
+    app.controller("StoreController", function(){
+        this.products = gems;
+    });
+
+    app.controller('PanelController', function(){
+        this.tab = 1;
+
+        this.selectTab = function(selectedTab){
+            this.tab = selectedTab;
+        };
+
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        }
+
+    });
+
+    var gems = 
+    [{
+        name:   "Azurite",
+        price:  2.95,
+        description: "some stuff",
+        canPurchase: true,
+        soldOut: false
+    },
+    {
+        name:   "Dodecahedron",
+        price:  2.95,
+        description: "some more stuff",
+        canPurchase: true,
+        soldOut: false
+    }]; 
+}());
+
